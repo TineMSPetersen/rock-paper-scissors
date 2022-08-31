a084018d5cf1926c385ed05cd27258a4da08e0e5
Eyenoom <70878089+TineMereteSPetersen@users.noreply.github.com>
2022-08-31 15:02:56 +0200

    Add values to playRound function
    Adding values to each condition in the playRound function to later be summed together as points

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     // The const is an array defining the options
-    const choice = [ "Rock", "Paper", "Scissors"];
+    const choice = ["Rock", "Paper", "Scissors"];
 
     // const random picks a random length from the above array
     const random = Math.floor(Math.random() * choice.length);
@@ -15,14 +15,18 @@ function playRound(playerSelection, computerSelection) {
             case 'Rock':
                 console.log("Computer uses Rock!")
                 console.log("A draw!");
+                return 0;
                 break;
             case 'Scissors':
                 console.log("Computer uses Scissors!")
                 console.log("You win!")
+                return 1;
                 break;
             case 'Paper':
                 console.log("Computer uses Paper!")
                 console.log("You lose!")
+
+                return -1;
                 break;
             default:
                 console.log("Something went wrong");
@@ -32,14 +36,20 @@ function playRound(playerSelection, computerSelection) {
             case 'Rock':
                 console.log("Computer uses Rock!")
                 console.log("You win!")
+
+                return 1;
                 break;
             case 'Paper':
                 console.log("Computer uses Paper!")
                 console.log("A draw!");
+
+                return 0;
                 break;
             case 'Scissors':
                 console.log("Computer uses Scissors!")
                 console.log("You lose!");
+
+                return -1;
             default:
                 console.log("Something went wrong");
         }
@@ -48,14 +58,20 @@ function playRound(playerSelection, computerSelection) {
             case 'Rock':
                 console.log("Computer uses Rock!")
                 console.log("You lose!");
+
+                return -1;
                 break;
-                case 'Scissors':
+            case 'Scissors':
                 console.log("Computer uses Scissors!")
                 console.log("A draw!");
+
+                return 0;
                 break;
             case 'Paper':
                 console.log("Computer uses Paper!")
                 console.log("You win!");
+
+                return 1;
                 break;
             default:
                 console.log("Something went wrong");
